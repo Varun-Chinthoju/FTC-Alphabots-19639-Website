f15d7b75f1c29cb7501a3c0e486ff8e8cef5f1ca
Varun Chinthoju <varun.chinthoju@gmail.com>
2026-03-05 15:54:52 -0800

    fix: revert Aadit Verma back to Outreach Captain for 24-25 as per user and bump cache

--- script.js
+++ script.js
@@ -371,7 +371,7 @@ document.addEventListener('DOMContentLoaded', () => {
         },
         {
             "name": "Aadit Verma",
-            "role": "Outreach Member",
+            "role": "Outreach Captain",
             "img": "images/team/2024_2025_aadit_verma.jpg"
         },
         {
